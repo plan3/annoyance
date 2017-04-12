0dd6c724ab6902786d97fd29b3eafd472b86ced3
Mårten Gustafson <marten.gustafson@schibsted.com>
2017-04-12 16:23:39 +0200

    Working code and accurate docs. It's a thing.

--- src/main/java/annoyance/model/Schedule.java
+++ src/main/java/annoyance/model/Schedule.java
@@ -10,10 +10,9 @@ public enum Schedule {
     daily, weekly;
 
     public Map<String, String> find(final Map<String, String> env) {
-        final String prefix = name().concat(":");
         return env.entrySet().stream()
-                .filter((job) -> job.getValue().startsWith(prefix))
-                .map((job) -> new SimpleEntry<>(job.getKey(), job.getValue().replaceFirst(prefix, "")))
+                .filter(job -> job.getKey().toLowerCase().startsWith(name().toLowerCase()))
+                .map(job -> new SimpleEntry<>(job.getKey(), job.getValue()))
                 .collect(toMap(Entry::getKey, Entry::getValue));
     }
 }
